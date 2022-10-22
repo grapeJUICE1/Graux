38eaa3cdb462dc2c4a04d17bab11f03cf6812bef
grapeJUICE1 <anonymyty123@gmail.com>
2022-10-22 20:15:07 +0600

    move typedefs from index to seperate folder

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,2 +1,30 @@
 const typeDefs = `#graphql
+  type User{
+   id: ID!
+   email: String!
+   firstName: String!
+   lastName: String!
+   username: String!
+    
+  }
+
+  type Battle {
+   id: ID!
+   uuid: String!
+   title: String!
+   votingTill: String!
+   winner : User
+   users: [User]
+    
+  }
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+
 `
+
+export default typeDefs
