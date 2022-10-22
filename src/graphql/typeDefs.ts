7c677e48f864acca695c4b99dc41d076d417ae40
grapeJUICE1 <anonymyty123@gmail.com>
2022-10-22 20:05:32 +0600

    update relations in entities

---
+++ src/graphql/typeDefs.ts
@@ -0,0 +1,2 @@
+const typeDefs = `#graphql
+`
--- src/index.ts
+++ src/index.ts
@@ -14,17 +14,28 @@ interface MyContext {
   token?: String
 }
 const typeDefs = `#graphql
- # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
     title: String
     author: String
   }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     books: [Book]
+  }
+
+
+  type User{
+   id: ID!
+   email: String!
+   firstName: String!
+   lastName: String!
+   username: String!
+    
+  }
+
+  type Battle {
+   id: ID!
+   uuid: String!
+   title: String!
   }
 `
 
