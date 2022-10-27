b61ee58989f3e2c88421260181fa6ed6178eb6c5
grapeJUICE1 <anonymyty123@gmail.com>
2022-10-27 15:38:02 +0600

    create BattleSong Entity

--- src/graphql/resolvers/battle/mutations.ts
+++ src/graphql/resolvers/battle/mutations.ts
@@ -1,52 +1,53 @@
 import dataSource from '../../../data-source'
 import Battle from '../../../entities/Battle'
 import User from '../../../entities/User'
+import addMiddleware from '../../../utils/addMiddleware'
+import isAuthMiddleware from '../../middlewares/isAuth'
 
 export default {
-  async createBattle(_, { title, votingTill, battleCreatedById, usersIdArr }) {
-    try {
-      if (!usersIdArr.includes(battleCreatedById)) {
-        usersIdArr.unshift(battleCreatedById)
-      }
-      const usersArr = []
-
-      for (const userId of usersIdArr) {
-        let user = await User.findOne({ where: { id: userId } })
-        if (!user) {
-          throw new Error('User Ids provided are faulty')
+  createBattle: addMiddleware(
+    isAuthMiddleware,
+    async (_, { title, votingTill, usersIdArr }, { payload }) => {
+      try {
+        if (!usersIdArr.includes(payload.userId)) {
+          usersIdArr.unshift(payload.userId)
         }
-        usersArr.push(user)
+        const usersArr = []
+
+        for (const userId of usersIdArr) {
+          let user = await User.findOne({ where: { id: userId } })
+          if (!user) {
+            throw new Error('User Ids provided are faulty')
+          }
+          usersArr.push(user)
+        }
+        const battleCreatedBy = usersArr[0]
+
+        const voteTill = new Date(Date.now() + votingTill * 3.6e6)
+
+        const battleRepository = dataSource.getRepository(Battle)
+
+        const newBattle = new Battle()
+
+        ;(newBattle.title = title),
+          (newBattle.votingTill = voteTill),
+          (newBattle.battleCreatedBy = battleCreatedBy),
+          (newBattle.users = usersArr)
+
+        battleRepository.save(newBattle)
+        // await Battle.insert({
+        //   title,
+        //   votingTill: voteTill,
+        //   battleCreatedBy,
+        //   users: usersArr,
+        // })
+
+        return true
+      } catch (err) {
+        throw new Error(err)
       }
-      const battleCreatedBy = usersArr[0]
-      //   await User.findOne({
-      //   where: { id: battleCreatedById },
-      // })
-      console.log(usersIdArr, usersArr)
-      const voteTill = new Date(Date.now() + votingTill * 3.6e6)
-
-      const battleRepository = dataSource.getRepository(Battle)
-
-      const newBattle = new Battle()
-
-      ;(newBattle.title = title),
-        (newBattle.votingTill = voteTill),
-        (newBattle.battleCreatedBy = battleCreatedBy),
-        (newBattle.users = usersArr)
-
-      battleRepository.save(newBattle)
-
-      // await Battle.insert({
-      //   title,
-      //   votingTill: voteTill,
-      //   battleCreatedBy,
-      //   users: usersArr,
-      // })
-
-      return true
-    } catch (err) {
-      throw new Error(err)
     }
-  },
+  ),
   async updateBattle() {},
   async deleteBattle() {},
   async voteForInBattle() {},
