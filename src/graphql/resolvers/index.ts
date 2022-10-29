673fd9c146bdc324396bad68883bcc248acc3fb3
grapeJUICE1 <anonymyty123@gmail.com>
2022-10-29 23:46:36 +0600

    setup for battleUser queries and mutations

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,15 +1,18 @@
 import { userQueries, userMutations } from './user'
 import { battleQueries, battleMutations } from './battle'
+import { voteQueries, voteMutations } from './vote'
 import { ResolverMap } from '../../types/graphql-utils'
 
 const resolvers: ResolverMap = {
   Query: {
     ...battleQueries,
     ...userQueries,
+    ...voteQueries,
   },
   Mutation: {
-    ...userMutations,
     ...battleMutations,
+    ...userMutations,
+    ...voteMutations,
   },
 }
 
---
+++ src/graphql/resolvers/vote/index.ts
@@ -0,0 +1,10 @@
+import Queries from './queries'
+import Mutations from './mutations'
+
+export const voteQueries = {
+  ...Queries,
+}
+
+export const voteMutations = {
+  ...Mutations,
+}
