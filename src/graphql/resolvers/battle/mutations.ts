d1ebef0a47505998ff30d783fadeb48f86fbb876
grapeJUICE1 <anonymyty123@gmail.com>
2022-10-29 14:38:34 +0600

    implement add battle user mutation

--- src/graphql/resolvers/battle/mutations.ts
+++ src/graphql/resolvers/battle/mutations.ts
@@ -1,4 +1,3 @@
-import dataSource from '../../../data-source'
 import Battle from '../../../entities/Battle'
 import User from '../../../entities/User'
 import addMiddleware from '../../../utils/addMiddleware'
@@ -24,8 +23,6 @@ export default {
 
         const expiresAt = new Date(Date.now() + expires * 3.6e6)
 
-        const battleRepository = dataSource.getRepository(Battle)
-
         const newBattle = new Battle()
 
         ;(newBattle.title = title),
@@ -33,7 +30,7 @@ export default {
           (newBattle.battleCreatedBy = battleCreatedBy),
           (newBattle.users = [battleCreatedBy])
 
-        await battleRepository.save(newBattle)
+        await Battle.save(newBattle)
 
         return newBattle
       } catch (err) {
@@ -41,68 +38,91 @@ export default {
       }
     }
   ),
-  async updateBattle(_, { id, title, winnerId }, { req }) {
-    try {
-      //Check if battle exists
-      const battle = await Battle.findOne({
-        where: { id },
-        relations: { users: true, battleCreatedBy: true, winner: true },
-      })
-      if (!battle) {
-        return new Error('Battle with the id does not exist')
+  updateBattle: addMiddleware(
+    isAuthMiddleware,
+    async (_, { id, title, winnerId }, { req }) => {
+      try {
+        //Check if battle exists
+        const battle = await Battle.findOne({
+          where: { id },
+          relations: { users: true, battleCreatedBy: true, winner: true },
+        })
+        if (!battle) {
+          return new Error('Battle with the id does not exist')
+        }
+
+        // Check if title is empty
+        if (!title || title.trim() === '') {
+          return new Error('Title cannot be empty')
+        }
+
+        // //update winner
+        // if (winnerId) {
+        //   // const checkIfWinnerInUsersArr = battle.users?.includes()
+        //   let winnerExistsInBattleUsers = false
+        //
+        //   const winner = await User.findOne({ where: { id: winnerId } })
+        //   if (!winner) {
+        //     return new Error('No user exists with the id of the winner')
+        //   }
+        //   if (battle.users) {
+        //     for (let user of battle.users) {
+        //       if (user.id === winner.id) {
+        //         winnerExistsInBattleUsers = true
+        //         break
+        //       }
+        //     }
+        //   }
+        //   if (!winnerExistsInBattleUsers) {
+        //     return new Error(
+        //       'User with that winner id does not exist among users participating in battle'
+        //     )
+        //   }
+        //
+        //   battle.winner = winner
+        // }
+
+        battle.title = title
+        await Battle.save(battle)
+        return battle
+      } catch (err) {
+        throw new Error(err)
       }
-
-      // Check if title is empty
-      if (!title || title.trim() === '') {
-        return new Error('Title cannot be empty')
-      }
-
-      // //update winner
-      // if (winnerId) {
-      //   // const checkIfWinnerInUsersArr = battle.users?.includes()
-      //   let winnerExistsInBattleUsers = false
-      //
-      //   const winner = await User.findOne({ where: { id: winnerId } })
-      //   if (!winner) {
-      //     return new Error('No user exists with the id of the winner')
-      //   }
-      //   if (battle.users) {
-      //     for (let user of battle.users) {
-      //       if (user.id === winner.id) {
-      //         winnerExistsInBattleUsers = true
-      //         break
-      //       }
-      //     }
-      //   }
-      //   if (!winnerExistsInBattleUsers) {
-      //     return new Error(
-      //       'User with that winner id does not exist among users participating in battle'
-      //     )
-      //   }
-      //
-      //   battle.winner = winner
-      // }
-
-      battle.title = title
-      await Battle.save(battle)
-      return battle
-    } catch (err) {
-      throw new Error(err)
     }
-  },
+  ),
 
   // add  a user
 
-  // async addBattleUser(_ , {id , usersIdArr} , context){
-  //   const battle = await Battle.findOne({where: {id}})
-  //   if(!battle) return new Error("Battle with that id does not exist")
-  //
-  //
-  // },
+  addBattleUser: addMiddleware(
+    isAuthMiddleware,
+    async (_, { battleId, newUserId }, { payload }) => {
+      const battle = await Battle.findOne({
+        where: { id: battleId },
+        relations: { battleCreatedBy: true, users: true },
+      })
+      if (!battle) return new Error('Battle with that id does not exist')
+
+      const userId = Number(payload.userId)
+      if (userId !== battle.battleCreatedBy.id)
+        return new Error('Battle was not created by you')
+
+      const userExistsInBattle = battle.users.find(
+        (user) => user.id === newUserId
+      )
+      if (userExistsInBattle) return new Error('User Already Exists in Battle')
+
+      const newUser = await User.findOne({ where: { id: newUserId } })
+      if (!newUser) return new Error('User with that Id does not exist')
+
+      battle.users.push(newUser)
+
+      await Battle.save(battle)
+
+      return battle
+    }
+  ),
 
   // delete a user
-
-  async deleteBattle() {},
 }
 
 //add mutation for adding users
