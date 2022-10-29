1fd3b849f44fb058c98902e4a2ce254281a963c8
grapeJUICE1 <anonymyty123@gmail.com>
2022-10-29 19:52:16 +0600

    check if title is unique in update battle mutation

--- src/graphql/resolvers/battle/mutations.ts
+++ src/graphql/resolvers/battle/mutations.ts
@@ -45,7 +45,7 @@ export default {
         //Check if battle exists
         const battle = await Battle.findOne({
           where: { id: battleId },
-          relations: { users: true, battleCreatedBy: true, winner: true },
+          relations: { users: true, battleCreatedBy: true },
         })
         if (!battle) {
           return new Error('Battle with the id does not exist')
@@ -55,37 +55,14 @@ export default {
           return new Error('Battle was not created by you')
 
         // Check if title is empty
-        if (!title || title.trim() === '') {
+        if (!title) {
           return new Error('Title cannot be empty')
         }
 
-        // //update winner
-        // if (winnerId) {
-        //   // const checkIfWinnerInUsersArr = battle.users?.includes()
-        //   let winnerExistsInBattleUsers = false
-        //
-        //   const winner = await User.findOne({ where: { id: winnerId } })
-        //   if (!winner) {
-        //     return new Error('No user exists with the id of the winner')
-        //   }
-        //   if (battle.users) {
-        //     for (let user of battle.users) {
-        //       if (user.id === winner.id) {
-        //         winnerExistsInBattleUsers = true
-        //         break
-        //       }
-        //     }
-        //   }
-        //   if (!winnerExistsInBattleUsers) {
-        //     return new Error(
-        //       'User with that winner id does not exist among users participating in battle'
-        //     )
-        //   }
-        //
-        //   battle.winner = winner
-        // }
-
-        battle.title = title
+        const battleExists = await Battle.findOne({ where: { title } })
+        if (battleExists)
+          return new Error('Battle with that title already exists')
+        battle.title = title.trim()
         await Battle.save(battle)
         return battle
       } catch (err) {
