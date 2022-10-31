bca14b9d05760d4a3bba9261379bf29b551d2c3f
grapeJUICE1 <anonymyty123@gmail.com>
2022-10-31 15:03:47 +0600

    rename folder vote to battleUser

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,6 +1,6 @@
 import { userQueries, userMutations } from './user'
 import { battleQueries, battleMutations } from './battle'
-import { voteQueries, voteMutations } from './vote'
+import { voteQueries, voteMutations } from './battleUser'
 import { ResolverMap } from '../../types/graphql-utils'
 
 const resolvers: ResolverMap = {
