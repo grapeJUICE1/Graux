01bf7f3c0d85dccab0bdf0f809649b023f062823
grapeJUICE1 <anonymyty123@gmail.com>
2022-11-01 12:42:05 +0600

    implement vote mutation

--- src/entities/VoteSubscriber.ts
+++ src/entities/VoteSubscriber.ts
@@ -24,13 +24,14 @@ export class VoteSubscriber implements EntitySubscriberInterface<Vote> {
       throw new Error(err)
     }
   }
+
   async beforeRemove(event: RemoveEvent<Vote>) {
     try {
       const voteCount = await Vote.count({
         relations: { battleUser: true },
         where: { battleUser: { id: event.entity.battleUser.id } },
       })
-      event.entity.battleUser.voteCount = voteCount
+      event.entity.battleUser.voteCount = voteCount - 1
       await BattleUser.save(event.entity.battleUser)
     } catch (err) {
       throw new Error(err)
