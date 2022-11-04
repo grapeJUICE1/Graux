0b463eddf98ae57169abe55e2687039c75aae520
grapeJUICE1 <anonymyty123@gmail.com>
2022-11-04 12:55:30 +0600

    implement getCommentsOfBattle mutation

--- src/graphql/resolvers/comment/queries.ts
+++ src/graphql/resolvers/comment/queries.ts
@@ -1,3 +1,4 @@
+import { GraphQLError } from 'graphql'
 import Comment from '../../../entities/Comment'
 
 export default {
@@ -9,5 +10,26 @@ export default {
       throw new Error(err)
     }
   },
-  getComment: async () => {},
+
+  getCommentsOfBattle: async (_: any, { battleId }) => {
+    try {
+      let errors = []
+      const comments = await Comment.find({
+        relations: { battle: { id: true }, user: true },
+        where: { battle: { id: battleId } },
+      })
+      if (!comments) {
+        errors.push({
+          path: 'comment',
+          message: 'Battle does not have any comments yet',
+        })
+        return new GraphQLError('Validation Error', {
+          extensions: { errors, code: 'BAD_USER_INPUT' },
+        })
+      }
+      return comments
+    } catch (err) {
+      throw new Error(err)
+    }
+  },
 }
