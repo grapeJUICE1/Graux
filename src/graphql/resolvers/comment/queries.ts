ab6d440fde1eddcf52538fd2f589431244eb7d3a
grapeJUICE1 <anonymyty123@gmail.com>
2022-11-04 12:48:00 +0600

    implement getComments mutation

--- src/graphql/resolvers/comment/queries.ts
+++ src/graphql/resolvers/comment/queries.ts
@@ -1,1 +1,13 @@
-export default {}
+import Comment from '../../../entities/Comment'
+
+export default {
+  getComments: async () => {
+    try {
+      const comments = await Comment.find({})
+      return comments
+    } catch (err) {
+      throw new Error(err)
+    }
+  },
+  getComment: async () => {},
+}
