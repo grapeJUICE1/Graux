a8ddb02e059fb540141ee156a8e04fae74ca4232
grapeJUICE1 <anonymyty123@gmail.com>
2022-11-04 12:55:52 +0600

    implement getCommentMutation

--- src/graphql/resolvers/comment/queries.ts
+++ src/graphql/resolvers/comment/queries.ts
@@ -32,4 +32,25 @@ export default {
       throw new Error(err)
     }
   },
+  getComment: async (_: any, { commentId }) => {
+    try {
+      let errors = []
+      const comment = Comment.findOne({
+        relations: { battle: true, user: true },
+        where: { id: commentId },
+      })
+      if (!comment) {
+        errors.push({
+          path: 'comment',
+          message: 'Comment with that id does not exist',
+        })
+        return new GraphQLError('Validation Error', {
+          extensions: { errors, code: 'BAD_USER_INPUT' },
+        })
+      }
+      return comment
+    } catch (err) {
+      throw new Error(err)
+    }
+  },
 }
