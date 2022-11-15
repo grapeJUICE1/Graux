8826ea09c6f3bd961b4024a25662407f4fdb33cd
grapeJUICE1 <anonymyty123@gmail.com>
2022-11-15 23:54:55 +0600

    fetch battles and render them in index page

--- client/apollo-client.ts
+++ client/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000',
+  uri: 'http://localhost:3000/graphql',
   cache: new InMemoryCache(),
 })
 
