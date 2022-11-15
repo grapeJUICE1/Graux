f2df0b5d60ebcb2d52cd0ef9a7dcfabfa97344c1
grapeJUICE1 <anonymyty123@gmail.com>
2022-11-15 22:51:12 +0600

    add and setup apollo client

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,11 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { ApolloProvider } from '@apollo/client'
+import client from '../apollo-client'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
   )
 }
