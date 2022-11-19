74ba69f9acbbe9ea9462d8daa146a029ee02319b
grapeJUICE1 <anonymyty123@gmail.com>
2022-11-19 20:13:11 +0600

    use formik and make login register form

--- client/components/Auth/Login.tsx
+++ client/components/Auth/Login.tsx
@@ -11,9 +11,30 @@ import {
   Text,
   useColorModeValue,
 } from '@chakra-ui/react'
+import { useFormik } from 'formik'
 import NextLink from 'next/link'
+import * as Yup from 'yup'
 
 function LoginCard() {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .min(3, 'Must be atleaast 3 characters long')
+        .max(30, "Can't exceed 30 characters")
+        .required('Required'),
+      password: Yup.string()
+        .min(8, 'Must be atleaast 8 characters long')
+        .max(255, "Can't exceed 255 characters")
+        .required('Required'),
+    }),
+    onSubmit: (values) => {
+      console.log(values)
+    },
+  })
   return (
     <Flex
       minH={'100vh'}
@@ -21,55 +42,65 @@ function LoginCard() {
       justify={'center'}
       bg={useColorModeValue('gray.50', 'gray.800')}
     >
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>Log in to your account</Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            to enjoy all of our cool features ✌️
-          </Text>
-        </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}
-        >
-          <Stack spacing={4}>
-            <FormControl id='email'>
-              <FormLabel>Email address</FormLabel>
-              <Input type='email' />
-            </FormControl>
-            <FormControl id='password'>
-              <FormLabel>Password</FormLabel>
-              <Input type='password' />
-            </FormControl>
-            <Stack spacing={10}>
-              <Button
-                bg={'blue.400'}
-                color={'white'}
-                mt={5}
-                _hover={{
-                  bg: 'blue.500',
-                }}
-              >
-                Log in
-              </Button>
-            </Stack>
-            <Stack pt={6}>
-              <Text align={'center'}>Don't Have and account?</Text>
-              <Link
-                //@ts-ignore
-                align={'center'}
-                as={NextLink}
-                href='/auth/register'
-                color={'blue.400'}
-              >
-                Register
-              </Link>
-            </Stack>
+      <form onSubmit={formik.handleSubmit}>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'4xl'}>Log in to your account</Heading>
+            <Text fontSize={'lg'} color={'gray.600'}>
+              to enjoy all of our cool features ✌️
+            </Text>
           </Stack>
-        </Box>
-      </Stack>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}
+          >
+            <Stack spacing={4}>
+              <FormControl id='username'>
+                <FormLabel>Your Username</FormLabel>
+                <Input type='text' {...formik.getFieldProps('username')} />
+                {formik.touched.username && formik.errors.username ? (
+                  <Text color='red.500'>{formik.errors.username}</Text>
+                ) : null}
+              </FormControl>
+              <FormControl id='password'>
+                <FormLabel>Password</FormLabel>
+                <Input type='password' {...formik.getFieldProps('password')} />
+                {formik.touched.password && formik.errors.password ? (
+                  <Text color='red.500'>{formik.errors.password}</Text>
+                ) : null}
+              </FormControl>
+              <Stack spacing={10}>
+                <Button
+                  isDisabled={!formik.isValid}
+                  type='submit'
+                  bg={'blue.400'}
+                  color={'white'}
+                  mt={5}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}
+                >
+                  Log in
+                </Button>
+              </Stack>
+              <Stack pt={6}>
+                <Text align={'center'}>Don't Have and account?</Text>
+                <Link
+                  //@ts-ignore
+                  align={'center'}
+                  as={NextLink}
+                  href='/auth/register'
+                  color={'blue.400'}
+                >
+                  Register
+                </Link>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </form>
     </Flex>
   )
 }
--- client/components/Auth/Register.tsx
+++ client/components/Auth/Register.tsx
@@ -17,10 +17,32 @@ import {
 import { useState } from 'react'
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
 import NextLink from 'next/link'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 function RegisterCard() {
   const [showPassword, setShowPassword] = useState(false)
-
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      username: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email().required('Required'),
+      username: Yup.string()
+        .min(3, 'Must be atleaast 3 characters long')
+        .max(30, "Can't exceed 30 characters")
+        .required('Required'),
+      password: Yup.string()
+        .min(8, 'Must be atleaast 8 characters long')
+        .max(255, "Can't exceed 255 characters")
+        .required('Required'),
+    }),
+    onSubmit: (values) => {
+      console.log(values)
+    },
+  })
   return (
     <Flex
       minH={'100vh'}
@@ -28,80 +50,86 @@ function RegisterCard() {
       justify={'center'}
       bg={useColorModeValue('gray.50', 'gray.800')}
     >
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'} textAlign={'center'}>
-            Register
-          </Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            to enjoy all of our cool features ✌️
-          </Text>
-        </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}
-        >
-          <Stack spacing={4}>
-            <HStack>
-              <Box>
-                <FormControl id='firstName' isRequired>
-                  <FormLabel>First Name</FormLabel>
-                  <Input type='text' />
-                </FormControl>
-              </Box>
-              <Box>
-                <FormControl id='lastName'>
-                  <FormLabel>Last Name</FormLabel>
-                  <Input type='text' />
-                </FormControl>
-              </Box>
-            </HStack>
-            <FormControl id='email' isRequired>
-              <FormLabel>Email address</FormLabel>
-              <Input type='email' />
-            </FormControl>
-            <FormControl id='password' isRequired>
-              <FormLabel>Password</FormLabel>
-              <InputGroup>
-                <Input type={showPassword ? 'text' : 'password'} />
-                <InputRightElement h={'full'}>
-                  <Button
-                    variant={'ghost'}
-                    onClick={() =>
-                      setShowPassword((showPassword) => !showPassword)
-                    }
-                  >
-                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-            <Stack spacing={10} pt={2}>
-              <Button
-                loadingText='Submitting'
-                size='lg'
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}
-              >
-                Register
-              </Button>
-            </Stack>
-            <Stack pt={6}>
-              <Text align={'center'}>
-                Already a user?{' '}
-                <Link as={NextLink} href='/auth/login' color={'blue.400'}>
-                  Login
-                </Link>
-              </Text>
-            </Stack>
+      <form onSubmit={formik.handleSubmit}>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'4xl'} textAlign={'center'}>
+              Register for a new account
+            </Heading>
+            <Text fontSize={'lg'} color={'gray.600'}>
+              to enjoy all of our cool features ✌️
+            </Text>
           </Stack>
-        </Box>
-      </Stack>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}
+          >
+            <Stack spacing={4}>
+              <FormControl id='email' isRequired>
+                <FormLabel>Email Address</FormLabel>
+                <Input type='email' {...formik.getFieldProps('email')} />
+                {formik.touched.email && formik.errors.email ? (
+                  <Text color='red.500'>{formik.errors.email}</Text>
+                ) : null}
+              </FormControl>
+              <FormControl id='username' isRequired>
+                <FormLabel>Your Username</FormLabel>
+                <Input type='text' {...formik.getFieldProps('username')} />
+                {formik.touched.username && formik.errors.username ? (
+                  <Text color='red.500'>{formik.errors.username}</Text>
+                ) : null}
+              </FormControl>
+              <FormControl id='password' isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                  <Input
+                    type={showPassword ? 'text' : 'password'}
+                    {...formik.getFieldProps('password')}
+                  />
+                  <InputRightElement h={'full'}>
+                    <Button
+                      variant={'ghost'}
+                      onClick={() =>
+                        setShowPassword((showPassword) => !showPassword)
+                      }
+                    >
+                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                {formik.touched.password && formik.errors.password ? (
+                  <Text color='red.500'>{formik.errors.password}</Text>
+                ) : null}
+              </FormControl>
+              <Stack spacing={10} pt={2}>
+                <Button
+                  loadingText='Submitting'
+                  size='lg'
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}
+                  type='submit'
+                  isDisabled={!formik.isValid}
+                >
+                  Register
+                </Button>
+              </Stack>
+              <Stack pt={6}>
+                <Text align={'center'}>
+                  Already a user?{' '}
+                  <Link as={NextLink} href='/auth/login' color={'blue.400'}>
+                    Login
+                  </Link>
+                </Text>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </form>
     </Flex>
   )
 }
