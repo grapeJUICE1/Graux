cff37fbbd7ec3c96e5b07bb86ac3f3ad9a726c64
grapeJUICE1 <anonymyty123@gmail.com>
2023-10-07 15:16:10 +0600

    add sort to query onChange in sort buttons

--- client/components/Buttons/SortButton.tsx
+++ client/components/Buttons/SortButton.tsx
@@ -1,9 +1,18 @@
 import { Select } from "@chakra-ui/react"
+import { useRouter } from "next/router"
+import { BaseSyntheticEvent, useState } from "react"
 
 function SortButton({ sortOptions }: { sortOptions: {} }) {
+  const router = useRouter()
+  const [value, setValue] = useState("createdAt")
+  const handleChange = (e: BaseSyntheticEvent) => {
+    console.log(e?.target?.value)
+    setValue(e?.target?.value)
+    router.push({ query: { ...router.query, sort: e?.target?.value } })
+  }
   return (
     <>
-      <Select>
+      <Select onChange={handleChange} value={value}>
         {Object.entries(sortOptions)?.map(([key, value], i) => (
           <option key={i} value={key}>
             {String(value)}
