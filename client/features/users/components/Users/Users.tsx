0f9cc50a237243ba50dacd4cea09c41acbbbb05d
grapeJUICE1 <anonymyty123@gmail.com>
2023-10-09 21:03:12 +0600

    made search working in users page

--- client/features/users/components/Users/Users.tsx
+++ client/features/users/components/Users/Users.tsx
@@ -2,6 +2,7 @@ import { Box, Center, Heading, SimpleGrid } from "@chakra-ui/react"
 import { useRouter } from "next/router"
 import SortButton from "../../../../components/Buttons/SortButton"
 import Pagination from "../../../../components/Pagination"
+import Search from "../../../../components/Search"
 import { User as UserType } from "../../../../gql/graphql"
 import UserCard from "../UserProfile/UserCard"
 
@@ -26,6 +27,13 @@ function Users({ initialTotal, initialUsers, pageSize }: UsersProps) {
   const onOrderByChange = (newOrderBy: string) => {
     router.push({ query: { ...router.query, page: 1, sort: newOrderBy } })
   }
+  const onSearch = (searchString: string | null) => {
+    router.push({
+      query: searchString
+        ? { page: 1, sort: "createdAt", search: searchString }
+        : { page: 1, sort: "createdAt" },
+    })
+  }
   return (
     <>
       <Heading textAlign="center" mt="5" pb="10">
@@ -40,7 +48,17 @@ function Users({ initialTotal, initialUsers, pageSize }: UsersProps) {
         </Box>
       </Center>
       <Center>
-        <SimpleGrid columns={{ base: 2, sm: 2, md: 3, xl: 4 }} spacing={10}>
+        <Box width="50%">
+          <Search onSearch={onSearch} placeholder="Search For Battles" />
+        </Box>
+      </Center>
+      <Center>
+        <SimpleGrid
+          mt={5}
+          pt={5}
+          columns={{ base: 2, sm: 2, md: 3, xl: 4 }}
+          spacing={10}
+        >
           {initialUsers?.map((user) => (
             <>
               <UserCard user={user} />
