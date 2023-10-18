5fe1d7ab6eef1104958a146c48da26fac75ccdac
grapeJUICE1 <anonymyty123@gmail.com>
2023-10-18 23:17:55 +0600

    used node env to use diff database in backend

--- src/config/config.ts
+++ src/config/config.ts
@@ -10,12 +10,19 @@ dotenv.config({ path: path.resolve(__dirname, "./config.env") })
 interface ENV {
   NODE_ENV: string | undefined
   PORT: number | undefined
-  DATABASE_HOST: string | undefined
   DATABASE_PORT: number | undefined
+
+  DATABASE_HOST: string | undefined
   DATABASE_USERNAME: string | undefined
   DATABASE_PASSWORD: string | undefined
   DATABASE_TYPE: string | undefined
   DATABASE_NAME: string | undefined
+
+  LOCAL_DATABASE_HOST: string | undefined
+  LOCAL_DATABASE_USERNAME: string | undefined
+  LOCAL_DATABASE_PASSWORD: string | undefined
+  LOCAL_DATABASE_NAME: string | undefined
+
   ACCESS_TOKEN_SECRET: string | undefined
   REFRESH_TOKEN_SECRET: string | undefined
   LAST_FM_API_KEY: string | undefined
@@ -30,6 +37,12 @@ interface Config {
   DATABASE_PASSWORD: string
   DATABASE_TYPE: string
   DATABASE_NAME: string
+
+  LOCAL_DATABASE_HOST: string
+  LOCAL_DATABASE_USERNAME: string
+  LOCAL_DATABASE_PASSWORD: string
+  LOCAL_DATABASE_NAME: string
+
   ACCESS_TOKEN_SECRET: string
   REFRESH_TOKEN_SECRET: string
   LAST_FM_API_KEY: string
@@ -49,6 +62,10 @@ const getConfig = (): ENV => {
     DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
     DATABASE_TYPE: process.env.DATABASE_TYPE,
     DATABASE_NAME: process.env.DATABASE_NAME,
+    LOCAL_DATABASE_HOST: process.env.LOCAL_DATABASE_HOST,
+    LOCAL_DATABASE_USERNAME: process.env.LOCAL_DATABASE_USERNAME,
+    LOCAL_DATABASE_PASSWORD: process.env.LOCAL_DATABASE_PASSWORD,
+    LOCAL_DATABASE_NAME: process.env.LOCAL_DATABASE_NAME,
     ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
     REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
     LAST_FM_API_KEY: process.env.LAST_FM_API_KEY,
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,24 +1,46 @@
-import 'reflect-metadata'
-import { DataSource } from 'typeorm'
-import config from './config/config'
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import config from "./config/config"
 
 const AppDataSource = new DataSource({
   //@ts-ignore
   type: config.DATABASE_TYPE,
-  host: config.DATABASE_HOST,
   port: config.DATABASE_PORT,
-  username: config.DATABASE_USERNAME,
-  password: config.DATABASE_PASSWORD,
-  database: config.DATABASE_NAME,
+
+  host:
+    process.env.NODE_ENV === "production"
+      ? config.DATABASE_HOST
+      : config.LOCAL_DATABASE_HOST,
+  username:
+    process.env.NODE_ENV === "production"
+      ? config.DATABASE_USERNAME
+      : config.LOCAL_DATABASE_USERNAME,
+  password:
+    process.env.NODE_ENV === "production"
+      ? config.DATABASE_PASSWORD
+      : config.LOCAL_DATABASE_PASSWORD,
+  database:
+    process.env.NODE_ENV === "production"
+      ? config.DATABASE_NAME
+      : config.LOCAL_DATABASE_NAME,
+
   synchronize: true,
   logging: false,
-  entities: ['src/entities/**/*.ts'],
-  migrations: ['src/migration/**/*.ts'],
-  subscribers: ['src/subscribers/**/*.ts'],
+  entities: ["src/entities/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscribers/**/*.ts"],
   cli: {
-    entitiesDir: 'src/entities',
-    migrationsDir: 'src/migration',
-    subscribersDir: 'src/subscribers',
+    entitiesDir: "src/entities",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscribers",
+  },
+  extra: {
+    ssl:
+      process.env.NODE_ENV === "production"
+        ? {
+            rejectUnauthorized: false,
+          }
+        : "",
   },
 })
 
