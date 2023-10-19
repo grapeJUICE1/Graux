336e43a6122a95068c6d28e9b9e1585d7d6bf991
grapeJUICE1 <anonymyty123@gmail.com>
2023-10-19 23:36:03 +0600

    config.env debug 1

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,7 @@ import path from "path"
 import dotenv from "dotenv"
 // Parsing the env file.
 if (process.env.NODE_ENV === "production") {
+  console.log(process.env)
   //dotenv.config({ path: "/etc/secrets/config.env" })
 } else {
   dotenv.config({ path: path.resolve(__dirname, "./config.env") })
